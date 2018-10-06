352d7e3dccf2e0877d4bd02294a5a44b48886523
Johan Plasencia <plasenciajohan@hotmail.com>
2018-10-06 11:30:05 +0100

    pruebas conduccion

---
+++ 80racer/Assets/Scripts/VehicleMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleMovement : MonoBehaviour {
+
+
+
+    public float speed = 90f;
+    public float turnSpeed = 5f;
+    public float hoverForce = 65f;
+    public float HoverHeight = 3.5f;
+
+
+    private float powerInput;
+    private float turnInput;
+    private Rigidbody carRigidbody;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        carRigidbody = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        powerInput = Input.GetAxis("Vertical");
+
+        turnInput = Input.GetAxis("Horizontal");
+		
+	}
+
+    void FixedUpdate() {
+
+        Ray ray = new Ray(transform.position,-transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, HoverHeight)) {
+
+            float proportionalHeight = (HoverHeight - hit.distance) / HoverHeight;
+            Vector3 appliedHoverForce = Vector3.up * proportionalHeight * hoverForce;
+            carRigidbody.AddForce(appliedHoverForce,ForceMode.Acceleration);
+
+    
+        }
+        carRigidbody.AddRelativeForce(0f, 0f, powerInput * speed);
+        carRigidbody.AddRelativeTorque(0f, turnInput * turnSpeed,0f);
+    }
+}
